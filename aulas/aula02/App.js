1193d57adf342fd5c12d1b2ea1fa4062288378bc
julia-b-142 <julia.barbosa.ads@gmail.com>
2025-03-14 21:09:29 -0300

    Praticando criação de Tela Home, galeria e de lista

--- aulas/aula02/App.js
+++ aulas/aula02/App.js
@@ -1,20 +1,13 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import Flexbox from "./screens/Flexbox";
+import GaleriaScreen from "./screens/GaleriaScreen";
+import PadraoAndroid from "./screens/PadraoAndroid";
+import PadraoIOS from "./screens/PadraoIOS";
+import PerfilScreen from "./screens/PerfilScreen";
 
-export default function App() {
+function App (){
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <PerfilScreen/>
   );
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+export default App;
---
+++ aulas/aula02/screens/PerfilScreen.js
@@ -0,0 +1,45 @@
+import {View, Text, StyleSheet} from "react-native"
+
+function PerfilScreen(){
+    return(
+        <View style={{flex:1}}>
+            <View style={styles.perfil}>
+                <View style={styles.photo}></View>
+                <View style={styles.card}>
+                    <Text>Nome</Text>
+                    <Text>E-mail</Text>
+                </View>
+            </View>
+            <View style={styles.skillSection}>
+                <Text>Habilidades</Text>
+                <View style={styles.skills}>
+                    <Text>Javascript</Text>
+                    <Text>React Native</Text>
+                </View>
+            </View>
+        </View>
+    )
+}
+
+const styles = StyleSheet.create({
+    perfil: {flexDirection: "row",
+    padding: 16
+    },
+    photo: {
+        height: 80,
+        width: 80,
+        backgroundColor: "lightgray",
+        borderRadius: 40
+    },
+    card:{
+        justifyContent: "center",
+        paddingLeft: 16
+    },
+    skillSection: { paddingLeft: 16},
+    skills: {
+        flexDirection: "row",
+        gap: 8
+    }
+})
+
+export default PerfilScreen;
