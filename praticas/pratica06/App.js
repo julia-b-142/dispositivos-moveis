a59457676bc389974a252925584fef13e7f1baa2
julia-b-142 <julia.barbosa.ads@gmail.com>
2025-04-07 20:56:37 -0300

    Criando tela de Tasks

---
+++ praticas/pratica06/App.js
@@ -0,0 +1,15 @@
+import { SafeAreaProvider } from "react-native-safe-area-context";
+import TaskScreen from "./screens/TaskScreen";
+import { TaskProvider } from "./contexts/TaskContext";
+
+function App() {
+  return (
+    <SafeAreaProvider>
+      <TaskProvider>
+        <TaskScreen />
+      </TaskProvider>
+    </SafeAreaProvider>
+  )
+}
+
+export default App;
